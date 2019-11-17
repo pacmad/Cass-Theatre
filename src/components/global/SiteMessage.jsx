60089f7b6a17d03ad647c4dce2959a71bfdc0153
Drew Markel <drewmrk8@gmail.com>
2019-11-17 13:04:31 -0500

    Remove unnecessary render function

--- src/components/global/SiteMessage.jsx
+++ src/components/global/SiteMessage.jsx
@@ -1,20 +1,17 @@
 import React from 'react'
 
 export const GlobalSiteMessage = props => {
-  const ShouldRender = () => {
-    if (props.message) {
-      return (
-        <span
-          className={`global-site-message ${
-            props.top ? 'global-site-message-top' : 'global-site-message-bottom'
-          }`}
-        >
-          <p className='global-site-message__text'>{props.message}</p>
-        </span>
-      )
-    } else {
-      return null
-    }
+  if (props.message) {
+    return (
+      <span
+        className={`global-site-message ${
+          props.top ? 'global-site-message-top' : 'global-site-message-bottom'
+        }`}
+      >
+        <p className="global-site-message__text">{props.message}</p>
+      </span>
+    )
+  } else {
+    return null
   }
-  return <ShouldRender />
 }
